f5aa1fc2f4c9088a6e41333b5b9e81509e0f5463
tonyyph <tonyphvincent@gmail.com>
2023-05-27 02:05:38 +0700

    feat:
    add firebase for authen

--- App.tsx
+++ App.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import { Box, UtilityThemeProvider } from 'react-native-design-utility';
 
 import { theme } from './src/constants/theme';
-// import MainNavigation from './src/navigations/MainNavigation';
+import MainNavigation from './src/navigations/MainNavigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-// import {
-//   RootStoreContext,
-//   RootStoreProvider,
-// } from './src/contexts/RootStoreContext';
-// import { rootStore } from './src/stores/RootStore';
+import {
+  RootStoreContext,
+  RootStoreProvider,
+} from './src/contexts/RootStoreContext';
+import { rootStore } from './src/stores/RootStore';
 
 // declare var global: {HermesInternal: null | {}};
 
 const App = () => {
   return (
-    // <RootStoreProvider rootStore={rootStore}>
-    <UtilityThemeProvider theme={theme}>
-      <SafeAreaProvider>
-        {/* <MainNavigation /> */}
-      </SafeAreaProvider>
-    </UtilityThemeProvider>
-    // </RootStoreProvider>
+    <RootStoreProvider rootStore={rootStore}>
+      <UtilityThemeProvider theme={theme}>
+        <SafeAreaProvider>
+          <MainNavigation />
+        </SafeAreaProvider>
+      </UtilityThemeProvider>
+    </RootStoreProvider>
   );
 };
 
